6011b2b758f82b14057b20b09b008fe00fb64753
Felix Orinda <forinda82@gmail.com>
2022-06-02 12:47:39 +0300

    Update build

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import * as shuffle from './utils';
+export default shuffle;
+export declare const alphabets: typeof shuffle.alphabets, capitalize: typeof shuffle.capitalize, generateRandomString: typeof shuffle.generateRandomString, lowerCaseLetters: string[], shuffleString: typeof shuffle.shuffleString, symbols: string[], upperCaseLetters: string[];
+//# sourceMappingURL=index.d.ts.map
--- lib/utils/index.d.ts
+++ lib/utils/index.d.ts
@@ -5,13 +5,5 @@ import { lowerCaseLetters } from './alphabet';
 import { shuffleString } from './shuffle-string';
 import { symbols } from './alphabet';
 import { upperCaseLetters } from './alphabet';
-
-export {
-	alphabets,
-	capitalize,
-	generateRandomString,
-	lowerCaseLetters,
-	upperCaseLetters,
-	shuffleString,
-	symbols,
-};
+export { alphabets, capitalize, generateRandomString, lowerCaseLetters, upperCaseLetters, shuffleString, symbols, };
+//# sourceMappingURL=index.d.ts.map
